007b0ff62abe5bc93c1f953ed9c30b2f7a3a5440
jt637 <justintornetta@gmail.com>
2024-04-24 20:17:08 -0400

    changed alias and added vi mode for tmux

--- config.sh
+++ config.sh
@@ -11,10 +11,10 @@ curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh | sh
 atuin import auto
 atuin_install="eval \"\$\(atuin init bash --disable-up-arrow)\" >> ~/.bashrc"
 
-source ~/.bashrc
+# source ~/.bashrc
 
 # set tmux config
-tmux="# set mouse mode on\nset -g mouse on\n\n# remap prefix to ctrl s\nunbind C-b\nset-option -g prefix C-s\nbind-key C-s send-prefix\n\n# split panes using | and -\nbind-key | split-window -h -c \"#{pane_current_path}\"\nbind-key - split-window -v -c \"#{pane_current_path}\"\n\n# fix the terminal color issue\nset -g default-terminal \"screen-256color\"\n\n# changes copy from [ to c\nbind-key -n F4 copy-mode\n
+tmux="# set mouse mode on\nset -g mouse on\n\n# remap prefix to ctrl s\nunbind C-b\nset-option -g prefix C-s\nbind-key C-s send-prefix\n\n# split panes using | and -\nbind-key | split-window -h -c \"#{pane_current_path}\"\nbind-key - split-window -v -c \"#{pane_current_path}\"\n\n# fix the terminal color issue\nset -g default-terminal \"screen-256color\"\n\n# changes copy from [ to c\nbind-key -n F4 copy-mode\nsetw -g mode-keys vi\n
 "
 echo $tmux > ~/.tmux.conf
 
@@ -22,6 +22,7 @@ echo $tmux > ~/.tmux.conf
 #sed s//alias ll='ls -lF'/g
 
 # set more alias'
+# alias battery='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|to\ full|percentage|time\ to|energy"'
 alias="\nalias gittree='git config --global alias.tree \"log --graph --decorate --pretty=oneline --abbrev-commit --all\"'"
 echo $alias >> ~/.bashrc
 
