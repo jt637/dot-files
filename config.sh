5f5326bf2c8604d7f0e30b9d7b4b47f3b985e6cd
jt637 <justintornetta@gmail.com>
2024-04-28 17:56:16 -0400

    changes to config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 # set tmux config
-tmux="# set mouse mode on\nset -g mouse on\n\n# remap prefix to ctrl s\nunbind C-b\nset-option -g prefix C-s\nbind-key C-s send-prefix\n\n# split panes using | and -\nbind-key | split-window -h -c \"#{pane_current_path}\"\nbind-key - split-window -v -c \"#{pane_current_path}\"\n\n# fix the terminal color issue\nset -g default-terminal \"screen-256color\"\n\n# changes copy from [ to c\nbind-key -n F4 copy-mode\n
+tmux="# set mouse mode on\nset -g mouse on\n\n# remap prefix to ctrl s\nunbind C-b\nset-option -g prefix C-s\nbind-key C-s send-prefix\n\n# split panes using | and -\nbind-key | split-window -h -c \"#{pane_current_path}\"\nbind-key - split-window -v -c \"#{pane_current_path}\"\n\n# fix the terminal color issue\nset -g default-terminal \"screen-256color\"\n\n# changes copy from [ to c\nbind-key -n F4 copy-mode\nsetw -g mode-keys vi\n
 "
 echo $tmux > ~/.tmux.conf
-
+tmux source ~/.tmux.conf
 # change some of the default alias'
 #sed s//alias ll='ls -lF'/g
 
