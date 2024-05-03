7e05c8bc9020e074d397bdce79c29f4db7528e97
jt637 <justintornetta@gmail.com>
2024-05-02 21:24:06 -0400

    updated to work with bash instead of sh

--- config.bash
+++ config.bash
@@ -18,7 +18,7 @@ done
 curl https://raw.githubusercontent.com/jt637/dot-files/main/alias.txt > /tmp/alias.txt
 while IFS= read -r line; do
     if grep -q -F "${line}" "$HOME/.bashrc"; then
-        echo "alias ${line} already in bashrc"
+        echo "${line} already in bashrc"
     else
         echo "$line" >> ~/.bashrc
     fi
@@ -27,14 +27,13 @@ done < "/tmp/alias.txt"
 # install atuin
 #curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh | /bin/bash
 atuin import auto
-atuin_install="eval \"\$\(atuin init bash --disable-up-arrow)\" >> ~/.bashrc"
 
-# source ~/.bashrc
+source ~/.bashrc
 
 # set tmux config
 tmux="# set mouse mode on\nset -g mouse on\n\n# remap prefix to ctrl s\nunbind C-b\nset-option -g prefix C-s\nbind-key C-s send-prefix\n\n# split panes using | and -\nbind-key | split-window -h -c \"#{pane_current_path}\"\nbind-key - split-window -v -c \"#{pane_current_path}\"\n\n# fix the terminal color issue\nset -g default-terminal \"screen-256color\"\n\n# changes copy from [ to c\nbind-key -n F4 copy-mode\nsetw -g mode-keys vi\n
 "
-echo $tmux > ~/.tmux.conf
+echo -e "$tmux" > ~/.tmux.conf
 tmux source ~/.tmux.conf
 # change some of the default alias'
 #sed s//alias ll='ls -lF'/g
