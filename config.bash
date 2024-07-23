f3647c012ce6ec8e128a421d63a7a56919011718
jt637 <justintornetta@gmail.com>
2024-07-22 20:54:05 -0400

    adding tpm and tmux resurrect

--- config.bash
+++ config.bash
@@ -122,11 +122,13 @@ $alias_list_cmd > ~/.bash_aliases
 source ~/.bashrc
 
 # set tmux config
-tmux="# set mouse mode on\nset -g mouse on\n\n# remap prefix to ctrl s\nunbind C-b\nset-option -g prefix C-s\nbind-key C-s send-prefix\n\n# split panes using | and -\nbind-key | split-window -h -c \"#{pane_current_path}\"\nbind-key - split-window -v -c \"#{pane_current_path}\"\n\n# fix the terminal color issue\nset -g default-terminal \"screen-256color\"\n\n# changes copy from [ to c\nbind-key -n F4 copy-mode\nsetw -g mode-keys vi\n
-"
+tmux="# set mouse mode on\nset -g mouse on\n\n# remap prefix to ctrl s\nunbind C-b\nset-option -g prefix C-s\nbind-key C-s send-prefix\n\n# split panes using | and -\nbind-key | split-window -h -c \"#{pane_current_path}\"\nbind-key - split-window -v -c \"#{pane_current_path}\"\n\n# fix the terminal color issue\nset -g default-terminal \"screen-256color\"\n\n# changes copy from [ to c\nbind-key -n F4 copy-mode\nsetw -g mode-keys vi\n\nset -g @plugin 'tmux-plugins/tpm'\nset -g @plugin 'tmux-plugins/tmux-sensible'\n\nset -g @plugin 'tmux-plugins/tmux-resurrect'\n\nrun '~/.tmux/plugins/tpm/tpm'"
+
 echo -e "$tmux" > ~/.tmux.conf
 # tmux source ~/.tmux.conf
-# change some of the default alias'
+
+# install tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2>/dev/null
 
 # log on success
 log_success "Config File Installation Completed Successfuly"
